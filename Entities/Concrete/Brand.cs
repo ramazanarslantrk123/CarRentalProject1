b649d75eb6d1b077f05ff6969a574020b38d2192
elifyenginol <elifyenginol5@gmail.com>
2021-02-22 23:31:53 +0300

    Ramazan <3

--- Entities/Concrete/Brand.cs
+++ Entities/Concrete/Brand.cs
@@ -10,5 +10,7 @@ namespace Entities.Concrete
         public int Id { get; set; }
         public string BrandName { get; set; }
         List<Car> Cars { get; set; }
+
+        //Ramazan Arslantürk
     }
 }
