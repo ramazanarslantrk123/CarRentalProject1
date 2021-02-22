40ac5d190aada0ea14a81f45fc52e5ddbc114164
Ramazan Arslantürk <ramazan.1997.1905@hotmail.com>
2021-02-22 23:27:36 +0300

    Add project files.

---
+++ Core/DataAccess/EntityFramework/EfGenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfGenericRepository<TEntity, TContext> : IGenericRepository<TEntity> where TEntity : class, IEntity, new() where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            using var context = new TContext();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using var context = new TContext();
+            context.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using var context = new TContext();
+            return context.Set<TEntity>().Where(filter).SingleOrDefault();
+        }
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using var context = new TContext();
+            if (filter != null)
+            {
+                return context.Set<TEntity>().Where(filter).ToList();
+            }
+            return context.Set<TEntity>().ToList();
+        }
+
+        public void Update(TEntity entity)
+        {
+            using var context = new TContext();
+            context.Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/RentalContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class RentalContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-4DC573S\\SQLEXPRESS;Database=RentalCarContext;Integrated Security=true");
+        }
+        DbSet<Car> Cars { get; set; }
+        DbSet<Brand> Brands { get; set; }
+        DbSet<Color> Colors { get; set; }
+    }
+}
